6123a9c090b3bc6457d52534ca79a2354bcfdd51
Vishnu Bhargavi Sabbisetty <bhargavi_sabbisetty@yahoo.in>
2020-03-04 17:16:01 +0530

    Modify code to support multiple endpoints

--- routes/bill.route.js
+++ routes/bill.route.js
@@ -1,9 +1,9 @@
-module.exports = (app) => {
+module.exports = (app,version) => {
     const billController = require('../controllers/bill.controller');
 
-    app.route('/v1/bills').get(billController.getAll); //this route helps to get all bills of a specific user
-    app.route('/v1/bill/:id').get(billController.getBillById); //this route helps to get bill by specific id
-    app.route('/v1/bill/:id').put(billController.put); //this route helps to update a specific bill
-    app.route('/v1/bill/').post(billController.post); //this route helps to create a new bill
-    app.route('/v1/bill/:id').delete(billController.deleteBillById); //this route helps to delete a specific bill
+    app.route('/'+version+'/bills').get(billController.getAll); //this route helps to get all bills of a specific user
+    app.route('/'+version+'/bill/:id').get(billController.getBillById); //this route helps to get bill by specific id
+    app.route('/'+version+'/bill/:id').put(billController.put); //this route helps to update a specific bill
+    app.route('/'+version+'/bill/').post(billController.post); //this route helps to create a new bill
+    app.route('/'+version+'/bill/:id').delete(billController.deleteBillById); //this route helps to delete a specific bill
 };
--- routes/file.route.js
+++ routes/file.route.js
@@ -1,7 +1,7 @@
-module.exports = (app) => {
+module.exports = (app,version) => {
     const fileController = require('../controllers/file.controller');
 
-    app.route('/v1/bill/:billId/file/:fileId').get(fileController.getBillAttachment); //this route helps to get attachment of a specific bill
-    app.route('/v1/bill/:id/file').post(fileController.post); //this route helps to create a new file for a bill
-    app.route('/v1/bill/:billId/file/:fileId').delete(fileController.deleteFileOfBill); //this route helps to delete a specific file
+    app.route('/'+version+'/bill/:billId/file/:fileId').get(fileController.getBillAttachment); //this route helps to get attachment of a specific bill
+    app.route('/'+version+'/bill/:id/file').post(fileController.post); //this route helps to create a new file for a bill
+    app.route('/'+version+'/bill/:billId/file/:fileId').delete(fileController.deleteFileOfBill); //this route helps to delete a specific file
 };
