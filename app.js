a260b153bac3fcdb50a9eba9e35322c6e52c9391
Vishnu Bhargavi Sabbisetty <bhargavi_sabbisetty@yahoo.in>
2020-03-15 03:12:40 +0530

    Demo

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 module.exports = function (app) {
     //providing routes context for our application
     let userRoutes = require('./routes/user.route');
-    userRoutes(app,'v1');
-    // userRoutes(app,'v2');
+    // userRoutes(app,'v1');
+    userRoutes(app,'v2');
 
     let billRoutes = require('./routes/bill.route');
-    billRoutes(app,'v1');
-    // billRoutes(app,'v2');
+    // billRoutes(app,'v1');
+    billRoutes(app,'v2');
 
     let fileRoutes = require('./routes/file.route');
-    fileRoutes(app,'v1');
-    // fileRoutes(app,'v2');
+    // fileRoutes(app,'v1');
+    fileRoutes(app,'v2');
 }
