4a7959cf5b81defff5dfa2b95f936aa5192c5340
Vishnu Bhargavi Sabbisetty <bhargavi_sabbisetty@yahoo.in>
2020-03-18 15:43:57 +0530

    configuration of logs and statsd-client

--- codedeploy/application_start.sh
+++ codedeploy/application_start.sh
@@ -4,7 +4,7 @@ ls
 pwd
 sudo npm i pm2 -g
 cd webapp
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-agent-config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch-agent-config.json -s
 sudo pm2 start server.js
 echo "hi"
 echo $MYSQL_DATABASE
