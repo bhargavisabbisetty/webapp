5a79344c630bc008294d1078c1317ee7e293f161
Vishnu Bhargavi Sabbisetty <bhargavi_sabbisetty@yahoo.in>
2020-03-26 07:27:43 +0530

    Update case

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
-const logger = require('../config/winston')
+const logger = require('./config/winston')
 const bodyParser = require('body-parser')
 const mysql = require('mysql');
 const basicAuth = require('./helpers/basic_authentication');
